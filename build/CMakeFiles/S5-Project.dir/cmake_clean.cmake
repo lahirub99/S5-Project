c00c25bd943563f3f01d8408254398df5afa23fe
Lahiru Bandara <lahirub@ceyentra.com>
2023-12-07 01:11:45 +0530

    build 1

---
+++ build/CMakeFiles/S5-Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/S5-Project.dir/main.cpp.obj"
+  "CMakeFiles/S5-Project.dir/main.cpp.obj.d"
+  "S5-Project.exe"
+  "S5-Project.exe.manifest"
+  "S5-Project.pdb"
+  "libS5-Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/S5-Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/S5-Project.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for S5-Project.
+# This may be replaced when dependencies are built.
